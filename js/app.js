3f6f845e096ceb09d992858187d56986ea89fc6a
aya-alasmar <aya.asmar32@gmail.com>
2021-01-18 23:50:33 +0200

    add lab 12 branch

--- js/app.js
+++ js/app.js
@@ -6,12 +6,18 @@ var rightImageElement = document.getElementById("right-image");
 var maxAttempts = 25;
 var userAttemptsCounter = 0;
 var leftImageIndex, middleImageIndex, rightImageIndex;
-
+var imagesNames = [];
+var imgVote = [];
+var statistics = [];
+var checkArray = [-1, -1, -1];
 function getImage(name, source) {
   this.name = name;
   this.source = source;
   this.votes = 0;
   this.repeat = 0;
+  this.shownPercentage = 0;
+  this.votePercentage = 0;
+  imagesNames.push(name);
   getImage.prototype.allImages.push(this);
 }
 
@@ -23,21 +29,14 @@ new getImage("boots", "img/boots.jpg");
 new getImage("breakfast", "img/breakfast.jpg");
 new getImage("bubblegum", "img/bubblegum.jpg");
 new getImage("chair", "img/chair.jpg");
-new getImage("cruisin-goat", "img/cruisin-goat.jpg");
 new getImage("cthulhu", "img/cthulhu.jpg");
 new getImage("dog-duck", "img/dog-duck.jpg");
 new getImage("dragon", "img/dragon.jpg");
-new getImage("goat-away", "img/goat-away.jpg");
-new getImage("goat-out-of-hand", "img/goat-out-of-hand.jpg");
 new getImage("pen", "img/pen.jpg");
 new getImage("pet-sweep", "img/pet-sweep.jpg");
-new getImage("sassy-goat", "img/sassy-goat.jpg");
 new getImage("scissors", "img/scissors.jpg");
 new getImage("shark", "img/shark.jpg");
-new getImage("smiling-goat", "img/smiling-goat.jpg");
-new getImage("sweater-goat", "img/sweater-goat.jpg");
 new getImage("sweep", "img/sweep.png");
-new getImage("shark", "img/shark.jpg");
 new getImage("tauntaun", "img/tauntaun.jpg");
 new getImage("unicorn", "img/unicorn.jpg");
 new getImage("usb", "img/usb.gif");
@@ -58,8 +57,6 @@ function handleUserClick(event) {
   userAttemptsCounter++;
 
   if (userAttemptsCounter <= maxAttempts) {
-    console.log(event.target);
-
     if (event.target.id === "left-image") {
       getImage.prototype.allImages[leftImageIndex].votes++;
     } else if (event.target.id === "middle-image") {
@@ -86,35 +83,68 @@ function userRoundTimes(event) {
 }
 function showResult(event) {
   event.preventDefault();
-  
+
   var resultsList = document.getElementById("results-list");
   resultsList.textContent = " ";
   var resultItem;
-  for (var i = 0; i <  getImage.prototype.allImages.length; i++) {
-     
+  percentage();
+  for (var i = 0; i < getImage.prototype.allImages.length; i++) {
     resultItem = document.createElement("li");
     resultItem.textContent =
-    getImage.prototype.allImages[i].name +
+      getImage.prototype.allImages[i].name +
       " had " +
       getImage.prototype.allImages[i].votes +
       " votes and " +
       getImage.prototype.allImages[i].repeat +
       " times";
     resultsList.appendChild(resultItem);
+    imgVote.push(getImage.prototype.allImages[i].votePercentage);
+    statistics.push(getImage.prototype.allImages[i].shownPercentage);
   }
+  // the chart
+  var ctx = document.getElementById("myChart").getContext("2d");
+  var chart = new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: imagesNames,
+      datasets: [
+        {
+          label: "Votes",
+          backgroundColor: "rgb(255, 99, 132)",
+          borderColor: "rgb(255, 99, 132)",
+          data: imgVote,
+        },
+
+        {
+          label: "showns",
+          backgroundColor: "#4b134f",
+          borderColor: "#4b134f",
+          data: statistics,
+        },
+      ],
+    },
+  });
 }
 
 function renderRandomImages() {
-  leftImageIndex = generateRandomIndex();
-
   do {
+    leftImageIndex = generateRandomIndex();
     rightImageIndex = generateRandomIndex();
     middleImageIndex = generateRandomIndex();
   } while (
     leftImageIndex == rightImageIndex ||
-    leftImageIndex === middleImageIndex ||
-    middleImageIndex === rightImageIndex
+    leftImageIndex == middleImageIndex ||
+    middleImageIndex == rightImageIndex ||
+    check(leftImageIndex) ||
+    check(middleImageIndex) ||
+    check(rightImageIndex)
   );
+
+  checkArray[0] = leftImageIndex;
+  checkArray[1] = rightImageIndex;
+  checkArray[2] = middleImageIndex;
+
+  console.log(leftImageIndex, rightImageIndex, middleImageIndex, "result");
 
   leftImageElement.src = getImage.prototype.allImages[leftImageIndex].source;
   getImage.prototype.allImages[leftImageIndex].repeat++;
@@ -127,4 +157,22 @@ function renderRandomImages() {
 
 function generateRandomIndex() {
   return Math.floor(Math.random() * getImage.prototype.allImages.length);
+}
+
+function percentage() {
+  for (var i = 0; i < getImage.prototype.allImages.length; i++) {
+    getImage.prototype.allImages[i].shownPercentage =
+      (getImage.prototype.allImages[i].repeat / maxAttempts) * 100;
+    getImage.prototype.allImages[i].votePercentage =
+      (getImage.prototype.allImages[i].votes / maxAttempts) * 100;
+  }
+}
+
+function check(imgIndex) {
+  for (var i = 0; i < checkArray.length; i++) {
+    if (imgIndex == checkArray[i]) {
+      return true;
+    }
+  }
+  return false;
 }
